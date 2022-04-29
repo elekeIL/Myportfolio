9819cf6c4df484f0226c3c44c14a53223c413e8b
unknown <you@example.com>
2022-04-29 03:56:39 +0100

    upadte to the code

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,9 +1,14 @@
 import Typed from "typed.js";
+import React from 'react'
 import { useEffect, useRef } from "react";
 import './Home.css'
 import { greeting } from '../Header/Greeting'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import PhoneIcon from '@mui/icons-material/Phone';
 
 function Home() {
   // Create Ref element.
@@ -27,18 +32,36 @@ function Home() {
   }, []);
 
   return (
-    <div className= 'home_container'>
+    <div className='home_container'>
+      <div className='square'></div>
+       <div className='square'></div>
+        <div className='square'></div>
+           <div className='square'></div>
+             <div className='square'></div>
+               <div className='square'></div>
+                  <div className='square'></div>
+                   <div className='square'></div>
+                     <div className='square'></div>
+      
       <div>
-      <h1> {greeting} <span>☺</span> </h1> 
-      <h2> welcome to my website </h2>
+        <h1> {greeting} <span>☺</span> </h1>
+        <h2> welcome to my website </h2>
 
-      {/* Element to display typing strings */}
-      <span className='dym_txt' ref={el}></span>
-     </div>
-     <Button>
-     <Link className='Main_page_link' to="/portfolio">Portfolio</Link>
-     </Button>
-     <p> Click on the link below to be taken to my portfolio webpage </p>
+        {/* Element to display typing strings */}
+        <span className='dym_txt' ref={el}></span>
+      </div>
+      <div className='social'>
+        <a href='https://twitter.com/IheanyiEleke' target='blank' alt='twitter'><TwitterIcon/></a>
+        <a href='https://github.com/elekeIL' target='blank' alt='GitHub'><GitHubIcon/></a>
+        <a href='https://www.linkedin.com/in/iheanyi-eleke/' target='blank' alt='linkedin'><LinkedInIcon/></a>
+        <a href='tel:+2347051156720' target='blank' alt='phone number'><PhoneIcon/></a>
+      </div>
+      <div className='button'>
+      <Button>
+        <Link className='Main_page_link' to="/portfolio">Portfolio</Link>
+      </Button>
+      <p> Click on the link below to be taken to my portfolio webpage </p>
+      </div>
     </div>
   );
 }
